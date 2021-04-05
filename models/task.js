1367b0ccb5287e104247d3e25b98315485bf4801
Evgeniya <evgeniyakirtoka@gmail.com>
2021-04-05 15:07:47 +0300

    Fix validation for task body
    delete isAlphanumeric check

--- models/task.js
+++ models/task.js
@@ -14,7 +14,6 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false,
         type: DataTypes.STRING,
         validate: {
-          isAlphanumeric: true,
           notNull: true,
           notEmpty: true,
         },
